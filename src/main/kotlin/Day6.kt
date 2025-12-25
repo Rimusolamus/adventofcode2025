f04310cf69687c6262bdb3ae2dc36ce4a197c277
Sergey Kalistratov <sergey.kalistratov@greencode.cz>
2025-12-25 13:31:17 +0100

    day 6 part 2

--- src/main/kotlin/Day6.kt
+++ src/main/kotlin/Day6.kt
@@ -1,33 +1,96 @@
 fun main() {
     val testLines = listOf(
-        "123 328  51 64",
-        "45 64  387 23",
-        "6 98  215 314",
-        "*   +   *   +"
+        "123 328  51 64  ",
+        " 45 64  387 23  ",
+        "  6 98  215 314 ",
+        "*   +   *   +   "
     )
     val lines = readInput("Day6Input")
 
-    val numbers = mutableListOf<List<String>>()
+    val numbers = mutableListOf<String>()
     for (line in lines) {
-        numbers.add(line.split(Regex("\\s+")))
+        numbers.add(line)
     }
 
-    var finalResult = 0L
-    for (i in 0..numbers.first().size - 1) {
-        if (numbers.last()[i] == "*") {
-            var result = 1L
-            for (numberIndex in 0..numbers.size - 2) {
-                result *= numbers[numberIndex][i].toInt()
+    var rowIndex = 0
+    var action: ACTION? = null
+    var result = 0L
+    val listOfReadyNumbers = mutableListOf<Long>()
+
+    while (true) {
+        if (rowIndex == numbers.first().length) {
+            // we are out of the string, it's time to go home
+            if (action == ACTION.SUM) {
+                result += sumAll(listOfReadyNumbers)
+                listOfReadyNumbers.clear()
+            } else if (action == ACTION.MULTIPLY) {
+                result += multiplyAll(listOfReadyNumbers)
+                listOfReadyNumbers.clear()
             }
-            finalResult += result
+            println("result is: $result")
+            break
         }
-        if (numbers.last()[i] == "+") {
-            var result = 0L
-            for (numberIndex in 0..numbers.size - 2) {
-                result += numbers[numberIndex][i].toInt()
+
+        // on empty line we do calculation then reset everything
+        if (areAllCharsAreWhitespaces(numbers, rowIndex)) {
+            if (action == ACTION.SUM) {
+                result += sumAll(listOfReadyNumbers)
+                listOfReadyNumbers.clear()
+            } else if (action == ACTION.MULTIPLY) {
+                result += multiplyAll(listOfReadyNumbers)
+                listOfReadyNumbers.clear()
             }
-            finalResult += result
+            rowIndex++
+            continue
         }
+
+        // if we found new action we change it
+        if (numbers.last().getOrNull(rowIndex) == '*') {
+            action = ACTION.MULTIPLY
+        }
+        if (numbers.last().getOrNull(rowIndex) == '+') {
+            action = ACTION.SUM
+        }
+
+        var number = ""
+
+        for (charIndex in 0..numbers.size - 2) {
+            if (!numbers[charIndex][rowIndex].isWhitespace()) {
+                number = number + numbers[charIndex][rowIndex].toString()
+            }
+        }
+        if (number != "") {
+            listOfReadyNumbers.add(number.toLong())
+        }
+
+        rowIndex++
     }
-    println(finalResult)
+}
+
+private fun sumAll(listOfReadyNumbers: List<Long>): Long {
+    var res = 0L
+    for (i in listOfReadyNumbers) {
+        res += i
+    }
+    return res
+}
+
+private fun multiplyAll(listOfReadyNumbers: List<Long>): Long {
+    var res = 1L
+    for (i in listOfReadyNumbers) {
+        res *= i
+    }
+    return res
+}
+
+private fun areAllCharsAreWhitespaces(numbers: MutableList<String>, rowIndex: Int): Boolean {
+    var counter = 0
+    for (i in 0..numbers.size-1) {
+        if (numbers[i].getOrNull(rowIndex)?.isWhitespace() == true) counter++
+    }
+    return counter == numbers.size
+}
+
+private enum class ACTION {
+    SUM, MULTIPLY
 }
